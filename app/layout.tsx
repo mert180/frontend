a26389a61d2169db0031cfa233058c5f32fbd14b
mert18 <mertuygur02@gmail.com>
2023-09-10 17:40:22 +0300

    font added (hepta slab)

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import "./globals.css";
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Hepta_Slab } from "next/font/google";
 
-const inter = Inter({ subsets: ["latin"] });
+const heptaSlab = Hepta_Slab({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Hire a Senior",
@@ -16,7 +16,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={heptaSlab.className}>{children}</body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,9 @@
 export default function Home() {
-  return <main></main>;
+  return (
+    <main className="h-screen w-screen bg-purple-darkest">
+      <div className="flex justify-center items-center h-full">
+        <h1 className="text-white text-4xl">Hire a Senior</h1>
+      </div>
+    </main>
+  );
 }
